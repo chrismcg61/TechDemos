aea9cd0f0564b0e6ccf1c96d9a84047b8f9bee23
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2017-09-02 16:39:41 +0200

    Create WebAudio.js

---
+++ MyLibs/WebAudio.js
@@ -0,0 +1,31 @@
+var audioContext = new (window.AudioContext || window.webKitAudioContext)();
+
+function onAudioFileLoaded( arrayBuf, sourceBuffers, sourceIndex ){	
+	audioContext.decodeAudioData( 
+  	arrayBuf, 
+  	function(decodedAudioBuffer){ onAudioDecoded( decodedAudioBuffer, sourceBuffers, sourceIndex ); }
+  );
+}
+function onAudioDecoded(decodedAudioBuffer, sourceBuffers, sourceIndex) {
+	sourceBuffers[0] = decodedAudioBuffer; 
+}
+
+
+
+function playDecodedBuffer(source, decodedAudioBuffer, start, detune) {
+	// Clear any existing audio source that we might be using:
+	if (source != null) {
+		source.disconnect(audioContext.destination);
+		source = null; // Leave existing source to garbage collection
+	} 
+	
+	// Recreate Buffer EVERY time you play a source with WebAudio API:
+	source = audioContext.createBufferSource();
+	source.buffer = decodedAudioBuffer; 
+	source.connect(audioContext.destination); 	
+	source.start( start );
+  
+  source.detune.value = detune * 100;		//pitch alteration (1 semitone avery 100 units, 12 semitones max up & down)
+  
+  return source;
+}
