17d59d812cbd85b0e9f9673fd2e8dd7654714ea1
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2017-12-06 16:50:38 +0100

    Create CorpCats.js

---
+++ MyLibs/Resume/Data/CorpCats.js
@@ -0,0 +1,124 @@
+////////////////////////////////////////////////////
+// CATEGORIES
+///////////////////////////////////////////////////
+var catIndex = 0;
+
+var categories = [
+  {
+    "index": catIndex++,
+    "title": "3D GAMES",
+    "rgb": "255,0,0",    
+    "jobs": [
+      {
+        "title" : "Solo Unity3D Dev",
+        "subJobs": [
+          "Js/C# Gameplay Scripts",
+          "Custom UI",          
+        ]
+      },
+      {
+        "title" : "Lead Custom Engine Dev",
+        "subJobs": [
+          "Console Drivers (GPU...)",
+          "Gameplay Libs", 
+          "3D Editor UI"
+        ]
+      },
+      {
+        "title" : "Lead Gameplay Dev",
+        "subJobs": [
+          "Controls Refactoring",
+          "In-Game UI",    
+          "AI (Bots,Strat)"
+        ]
+      },
+      {
+        "title" : "Tools Expert",
+        "subJobs": [
+          "Media Processing",
+          "Multi-PC Process",          
+        ]
+      },
+      
+    ]
+  },
+  
+  {
+    "index": catIndex++,
+    "title": "SetTopBox",
+    "rgb": "255,0,80",
+    "jobs": [
+      {
+        "title" : "Middleware Dev",
+        "subJobs": [
+          "HW Drivers (GPU, TV, HDD...)",
+          "WebApp / UI",          
+        ]
+      },
+      {
+        "title" : "Tools Expert",
+        "subJobs": [
+          "Data Processing",
+          "Stress Tests",
+          "Docs Compilation"
+        ]
+      },
+      {
+        "title" : "Perf/Quality Expert",
+        "subJobs": [
+          "Perf Monitoring",
+          "Refactoring",  
+          "Custom Libs",
+          "Docs",
+        ]
+      },
+      
+    ]
+  },
+  
+  {
+    "index": catIndex++,
+    "title": "Embedded / Robots",
+    "rgb": "120,120,255",
+    "jobs": [
+      {
+        "title" : "Middleware Dev",
+        "subJobs": [
+          "HW Drivers (GPU, TV, HDD...)",
+          "WebApp / UI",          
+        ]
+      },
+      {
+        "title" : "Job2",
+        "subJobs": [
+          "2.1",
+          "2.2",
+        ]
+      },
+      
+    ]
+  },
+
+  {
+    "index": catIndex++,
+    "title": "Other",
+    "rgb": "80,80,80",
+    "jobs": [
+      {
+        "title" : "Network Tools",
+        "subJobs": [
+          "1.1",
+          "1.2",          
+        ]
+      },
+      {
+        "title" : "Security / SSO WebApp",
+        "subJobs": [
+          "2.1",
+          "2.2",
+        ]
+      },
+
+    ]
+  },
+];
