18f4eea7113dacd778cf7577edebdf6946fa247a
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2017-12-06 16:19:18 +0100

    Create ResumeFuncs.js

---
+++ MyLibs/Resume/ResumeFuncs.js
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////
+// AUTO DATE VALUES:
+const msByYear = 365.2422 * 24 * 60 * 60 * 1000;
+const curYearDecimal = 1970 + Date.now() / msByYear;
+
+var myBirthYearDecimal = 1983 + 310/365;  // 6 Nov = 310th Day
+var myGraduationYearDecimal = 2007 + 0/365;  
+
+var myAge = curYearDecimal - myBirthYearDecimal;
+myAge = Math.floor( myAge );
+
+var myXP = curYearDecimal - myGraduationYearDecimal;
+myXP = Math.floor( myXP );
