2711175b3b4a298670e588345d3a0c8f094a1be8
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2017-12-06 16:47:51 +0100

    Create Corps.js

---
+++ MyLibs/Resume/Data/Corps.js
@@ -0,0 +1,157 @@
+////////////////////////////////////////////////////
+// CORPs
+///////////////////////////////////////////////////
+var startDate = 2006 + 2/4;
+var curDate = startDate;
+
+
+var corps = [
+  {
+    "name": "Dassault Aviation",
+    "location": "Istres (Marseille)",
+    "category": 3,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(3),
+             "end": {"f":curDate}, 
+            },    
+  },
+  
+  
+  {
+    "name": "Etranges Libellules",
+    "location": "Lyon",
+    "category": 0,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(3),
+             "end": {"f":curDate}, 
+            },    
+  },
+  {
+    "name": "Ubisoft / Phoenix Interactive",
+    "location": "Lyon",
+    "category": 0,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(9),
+             "end": {"f":curDate}, 
+            },    
+  },
+  
+  {
+    "name": "Sword",
+    "location": "Lyon",
+    "category": 3,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(1),
+             "end": {"f":curDate}, 
+            },    
+  },
+  
+  {
+    "name": "NeoPost",
+    "location": "Paris",
+    "category": 2,
+    "alten": true,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(2),
+             "end": {"f":curDate}, 
+            },    
+  },
+  {
+    "name": "Nagra (Canal+)",
+    "location": "Paris",
+    "category": 3,
+    "alten": true,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(1),
+             "end": {"f":curDate}, 
+            },    
+  },
+  
+  {
+    "name": "Soft@Home (Orange)",
+    "location": "Paris",
+    "category": 1,
+    "alten": true,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(5),
+              "end": {"f":curDate}, 
+            },    
+  },
+  
+  {
+    "name": "Technicolor",
+    "location": "Paris",
+    "category": 3,
+    "alten": true,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(1),
+              "end": {"f":curDate}, 
+            },    
+  },
+  {
+    "name": "Sagemcom",
+    "location": "Paris",
+    "category": 3,
+    "alten": true,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(1),
+              "end": {"f":curDate}, 
+            },    
+  },
+  
+  {
+    "name": "Bouygues Telecom",
+    "location": "Paris",
+    "category": 1,
+    "alten": true,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(4),
+             "end": {"f":curDate}, 
+            },    
+  },
+  
+  {
+    "name": "Parrot",
+    "location": "Paris",
+    "category": 2,
+    "alten": true,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(1),
+             "end": {"f":curDate}, 
+            },    
+  },
+  
+  {
+    "name": "Cisco",
+    "location": "Paris",
+    "category": 1,
+    "alten": true,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(5),
+             "end": {"f":curDate}, 
+            },    
+  },
+  
+  {
+    "name": "[PERSO] Unity3D Proj",
+    "location": "Paris",
+    "category": 0,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(4),
+             "end": {"f":curDate}, 
+            },    
+  },
+  {
+    "name": "[PERSO] WebApp/Game Proj",
+    "location": "Normandy",
+    "category": 0,
+    "date": {"start": {"f":curDate}, 
+             "duration": newDuration(4),
+             "end": {"f":curDate}, 
+            },    
+  },
+  
+  
+  
+  
+];
