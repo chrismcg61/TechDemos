7da727bef0372311f42ed64656cff0f366d1928c
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2017-12-06 16:54:13 +0100

    Create Jobs.js

---
+++ MyLibs/Resume/Data/Jobs.js
@@ -0,0 +1,60 @@
+////////////////////////////////////////////////////
+// JOBS
+///////////////////////////////////////////////////
+var jobStyles = [
+  {"font-weight": "bold"},
+  {"font-style": "normal"},
+  {"font-size": "small"},
+]
+
+var jobs = [
+  {
+    "mainTitle": "Senior Embedded / Middleware Prog.",
+    "subTitles": ["HW Drivers", "Software Libs", "UI",],
+    "style": jobStyles[0]
+  },
+  {
+    "mainTitle": "3D Prog.",
+    "smallTitle": "CPU, GPU",
+    "subTitles": ["Embedded","WebApps","Games"],
+    "style": jobStyles[0]
+  },
+  
+  {
+    "mainTitle": "WebApps Prog.",
+    "subTitles": ["Modular", "Embedded","3D",],
+    "style": jobStyles[0]
+  },
+  {
+    "mainTitle": "Games Prog.",
+    "subTitles": ["Unity3D / Custom UI", "Gameplay", "Custom Engine Drivers / Libs", "Custom Engine UI", "Data Processing Tools",],
+    "style": jobStyles[0],
+  },
+  {
+    "mainTitle": "SetTopBox Prog.",
+    "subTitles": ["Middleware HW Drivers", "Middleware Software / Libs", "Embedded WebApp",],
+    "style": jobStyles[0]
+  },
+ 
+  
+  
+  {
+    "mainTitle": "GPU Parallel Prog",
+    "smallTitle": "CPU Scripts Acceleration",
+    "subTitles": ["GLSL Shaders", "High Recursion Scripts"],
+    "style": jobStyles[1]
+  },
+  {
+    "mainTitle": "AI Prog",
+    "subTitles": ["Game Strat", "Game Bots", "Embedded/Robot", "Neural Networks"],
+    "style": jobStyles[1]
+  },
+  
+  {
+    "mainTitle": "Junior Teacher",
+    "smallTitle": "Maths, Prog",
+    "subTitles": ["Graduate Maths", "Game/WebApp/Embedded Prog",],
+    "style": jobStyles[2]
+  },
+
+];
