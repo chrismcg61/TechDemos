08b3a10ba4434a025189237eee6a64a31af15fd0
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2020-06-21 19:05:45 +0200

    Only ThreeJS Inits & Clean "initDefault()" Call

--- MyLibs/Three/MyThreeMin.js
+++ MyLibs/Three/MyThreeMin.js
@@ -1,95 +1,35 @@
 /////////////////////////////////////////////////////
-// User Vars :
-/////////////////////////////////////////////////////
-var gpuMode = 0;
-var staticParamFolders = {
-  ReInitRenderer:
-  {
-    Title:"ReInit Renderer",
-    Params:{
-      ReinitRender: initRenderer,
-    }
-  },
-
-};
-var dynParamFolders = {  }
-
-/////////////////////////////////////////////////////
 // Global Vars :
 /////////////////////////////////////////////////////
-var container, infoDiv;
-var camera, scene, renderer;
-var stats;
-
+var gpuMode = 1;
+var container,  camera, scene, renderer;
+//var infoDiv, stats;
 /////////////////////////////////////////////////////
 // First Time Inits :
 /////////////////////////////////////////////////////
-function initDivs() {
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+//
+function initMain() {
+  //if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+  document.body.style.margin = 0;
+  document.body.style.overflow = "hidden"; 
+  
   // 3D Container :
   container = document.createElement( 'div' );
   document.body.appendChild( container );
-
-  // Info Div:
-  infoDiv = document.createElement( 'div' );
-  document.body.appendChild( infoDiv );
-
-  //Styles:
-  infoDiv.style.position = "absolute";
-  infoDiv.style.top = 0;
-  infoDiv.style.left = "33%";
-  infoDiv.style.width = "33%";
-  infoDiv.style.textAlign = "center";
-  infoDiv.style.color = "#ffffff";
-  infoDiv.style.backgroundColor = "rgba(0,0,0, 0.8)";
-
-  document.body.style.margin = 0;
-  document.body.style.overflow = "hidden"; 
-
-}
-//
-function initGUI( updateCallback ) {    
-  var gui = new dat.GUI();
-  gui.open();  
-
-  var staticFolder = gui.addFolder("Static Params");
-  staticFolder.open();  
-  //for ( var i = 0; i < staticParamFolders.length; i ++ ) {  
-  for(var folderKey in staticParamFolders){
-    var newFolder = staticFolder.addFolder(staticParamFolders[folderKey].Title);
-    newFolder.open();
-    for(var key in staticParamFolders[folderKey].Params){
-      newFolder.add( staticParamFolders[folderKey].Params, key);    
-    } 
-  }
-
-  var dynFolder = gui.addFolder("Dynamic Params");
-  dynFolder.open();  
-  for(var folderKey in dynParamFolders){
-    var newFolder = dynFolder.addFolder(dynParamFolders[folderKey].Title);
-    newFolder.open();
-    for(var key in dynParamFolders[folderKey].Params){
-      newFolder.add( dynParamFolders[folderKey].Params, key).onChange( updateCallback );    
-    } 
-  }
-
-}
-//
-function initGeneric() {
-  //if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
-
-  // CAMERA
+  
+   // CAMERA
   camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.set( 2, 0, 2 );
-  camera.lookAt( new THREE.Vector3() );
+  //camera.position.set( 2, 0, 2 );
+  //camera.lookAt( new THREE.Vector3() );
 
   // Window Events:
   window.addEventListener( 'resize', onWindowResize, false );
-
-  // STATS
-  stats = new Stats();
-  container.appendChild( stats.dom );
 }
-
 /////////////////////////////////////////////////////
 // Renderer & Scene Inits :
 /////////////////////////////////////////////////////
@@ -98,11 +38,11 @@ function initRenderer() {
 
   if(gpuMode)   {
     renderer = new THREE.WebGLRenderer( { antialias: false } );
-    infoDiv.innerHTML = "Mode: GPU";
+    //infoDiv.innerHTML = "Mode: GPU";
   }
   else  {
     renderer = new THREE.CanvasRenderer();
-    infoDiv.innerHTML = "Mode: CPU";
+    //infoDiv.innerHTML = "Mode: CPU";
   }
 
   renderer.setPixelRatio( window.devicePixelRatio );
@@ -115,11 +55,6 @@ function initRenderer() {
 
   gpuMode = !gpuMode;
 }
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-}
 //
 function initScene( callback ){
   // SCENE
@@ -128,17 +63,25 @@ function initScene( callback ){
   //scene.fog = new THREE.Fog( 0x040306, 10, 300 ); 
 
   callback(); 
+  //setTimeout(callback, 1);
 }
-
 ////////////////////////////////////////
 // ANIMATE & RENDER :
 ////////////////////////////////////////
 function render( animateCallback ) {
-  stats.update();
-
+  //stats.update();
   animateCallback();
   renderer.render( scene, camera );
 
   requestAnimationFrame( function(){render(animateCallback);} );
+}
+////////////////////////////////////////
+// DEFAULT Init Func :
+////////////////////////////////////////
+function initDefault( myInitScene, myAnimScene ){
+  initMain();
+  initRenderer();
+  initScene( myInitScene );
+  render( myAnimScene );  
 }
 
