9b2067bedc88abce0a47a76f15a800006333d946
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2020-06-26 12:59:06 +0200

    Added "ColorObj" Gui Mgt

--- MyLibs/GUI.js
+++ MyLibs/GUI.js
@@ -8,15 +8,27 @@ function initGUI( ) {
     folder1 = gui.addFolder("Global Vars");
     folder1.open();
     for(var key in gVars){
-      folder1.add( gVars, key);    
+      // folder1.add( gVars, key);  
+      addGuiVar(folder1, gVars, key);
     }    
   }
   if (typeof vars !== 'undefined') {
     folder2 = gui.addFolder("Main Vars");
     folder2.open();
     for(var key in vars){
-      folder2.add( vars, key);    
+      // folder2.add( vars, key); 
+      addGuiVar(folder2, vars, key);
     }    
   }   
+}
+//////////////////////////////////////////////////////////////
+function addGuiVar(folder, vars, key){
+  var param = vars[key];
+  if( typeof(param) === 'object'){
+    var subFolder = folder.addFolder(key);
+    subFolder.addColor( param, "rgb");  
+    subFolder.add( param, "a");  
+  }
+  else folder.add( vars, key);  
 }
 //////////////////////////////////////////////////////////////
