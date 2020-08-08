3729d87af4b3d04c3e85deb96c2c5557a62fd65d
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2020-08-08 20:31:58 +0200

    Create BtnList.js

---
+++ MyLibs/Three/BtnList.js
@@ -0,0 +1,34 @@
+
+var btnList = [];
+var curBtnId = 0;
+//
+function initBtnList(btnGrp){
+  btnList = [];
+  curBtnId = 0;
+  //
+  btnList = btnGrp.getElementsByTagName("BUTTON");  
+  //
+  focusNextBtn(0);
+}
+//
+function focusNextBtn(i){
+  var nextBtnId = curBtnId+i;
+  if(nextBtnId >= btnList.length) nextBtnId=0;
+  else if(nextBtnId < 0) nextBtnId = btnList.length-1;
+  //
+  curBtnId = nextBtnId;
+  btnList[curBtnId].focus();
+}
+//
+function onMenuKeyDown( evt ) {
+  switch ( evt.keyCode ) {      
+    case 98: // PAD-down
+    case 102: // PAD-right
+      focusNextBtn(1);
+      break;
+    case 100: // PAD-left    
+    case 104: // PAD-up
+      focusNextBtn(-1);
+      break;          
+  }
+};
