ec20da53599c8e9cee9d6ef29d013f0165de4b11
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2020-08-10 20:17:45 +0200

    Create Table.js

---
+++ MyLibs/Three/Table.js
@@ -0,0 +1,163 @@
+var curTable, tableContainer;
+function hideColumn(colId) {  
+  if(curTable.rows[0].cells[colId].style.width != "auto") 
+    curTable.rows[0].cells[colId].style.width = "auto";
+  else
+    curTable.rows[0].cells[colId].style.width = "20px";    
+}
+
+
+var sortDir = 1;
+function sortTable(colId) {  
+  var rows, switching, i, x, y, shouldSwitch;
+  // var table = curTable;
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = curTable.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[colId];
+      y = rows[i + 1].getElementsByTagName("TD")[colId];
+      
+      // if(sortDir==-1){var tx = x;  x = y;  y = tx; } 
+      // var xVal = x.innerHTML;
+      var xVal = x.getElementsByTagName("INPUT")[0].value;
+      var yVal = y.getElementsByTagName("INPUT")[0].value;      
+      if(sortDir==-1) {
+        xVal = y.getElementsByTagName("INPUT")[0].value;  
+        yVal = x.getElementsByTagName("INPUT")[0].value;  
+      }
+      
+      //check if the two rows should switch place:
+      // if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+      if(parseInt(xVal) > parseInt(yVal)){
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+  
+  sortDir *= -1;
+}
+
+
+
+const canvasW = 100;
+refreshTable();
+function refreshTable() {  
+  //maxVal = parseInt(inputMax.value);
+  refreshBars();
+  
+  setTimeout(refreshTable, 500);
+}
+function refreshBars() {  
+  //var table = document.getElementById("myTable");
+  var rows = curTable.rows;
+  for (var colId = 1; colId < (rows[0].cells.length); colId++) {
+    refreshColBars(colId);
+  }        
+}
+function refreshColBars(colId) {
+  var rows = curTable.rows;
+  var maxColVal = 0;
+  /* Find MAX Col Value : */
+  for (var i = 1; i < (rows.length); i++) {
+    var cell = rows[i].getElementsByTagName("TD")[colId];
+    var cellVal = parseInt( cell.getElementsByTagName("INPUT")[0].value );
+    if(cellVal > maxColVal) maxColVal = cellVal;
+  }
+  /* Refresh Bars Accordingly : */
+  for (var i = 1; i < (rows.length); i++) {
+    var cell = rows[i].getElementsByTagName("TD")[colId];
+    var cellVal = cell.getElementsByTagName("INPUT")[0].value;
+    var bar = cell.getElementsByTagName("CANVAS")[0];
+    var barBack = cell.getElementsByTagName("CANVAS")[1];
+    refreshBar(bar, barBack, cellVal, maxColVal);
+  }
+}
+function refreshBar(bar, barBack, cellVal,maxColVal) {
+  if(cellVal<=maxColVal){
+    bar.width = cellVal * canvasW/maxColVal;
+    barBack.width = (maxColVal-cellVal) * canvasW/maxColVal;    
+  }    
+  else{
+    bar.width = cellVal;
+    barBack.width = 0;    
+  }     
+}
+
+
+
+function createTable(items, titleKey, paramsKey){
+  var params0 = items[0][paramsKey];
+  var newTable = document.createElement("TABLE");  
+  for ( var i=-1; i<items.length; i++ ) {
+    var item;
+    if(i==-1) item = items[0];
+    else item = items[i];
+    //
+    var params = item[paramsKey];
+    if(!params) continue;
+    /* Add Title to Params : */
+    if(!params[titleKey]) params[titleKey] = item[titleKey];
+    //
+    // var newTr = addRow(newTable);   
+    var newTr = addElt(newTable, "TR", "", "");
+    var colId = -1;
+    for(var key in params0){
+      colId++;
+      if(i==-1) {
+        var cell = addElt(newTr, "TH", "", "");
+        var btn = addElt(cell, "BUTTON", "", "+");
+        btn.onclick = setFunc(hideColumn,colId);      
+        btn.classList.add("minBtn");
+        var colName = addElt(cell, "SPAN", "", key);
+        var btn = addElt(cell, "BUTTON", "", "▼");
+        btn.onclick = setFunc(sortTable,colId);
+        btn.classList.add("minBtn");
+      }
+      else{ 
+        // addCell(newTr, "TD", params[key] );
+        if(key==titleKey) var cell = addElt(newTr, "TD", "", params[key]);
+        else{
+          var cell = addElt(newTr, "TD", "", "");
+          var newInput = addElt(cell, "INPUT", "", "");
+          newInput.type = "number";
+          newInput.classList.add("bigFont");
+          newInput.style.width = "50px";
+          newInput.value = params[key];
+          var canvas = addElt(cell, "CANVAS", "", "");
+          canvas.classList.add("barCanvas");
+          canvas.height = 8;
+          canvas.width = 100;
+          canvas.style.backgroundColor = "red";
+          canvas.style.marginLeft = "8px";
+          var canvas = addElt(cell, "CANVAS", "", "");
+          canvas.classList.add("barCanvas");
+          canvas.height = 8;
+          canvas.width = 100;
+          // canvas.style.backgroundColor = "red";
+        }
+      }
+    }
+  }
+  // meshBis.content = newTable;  
+  return newTable;
+}
+
