cf9f8e89fab4ea5eaa0b90a95d5e72b09739bdf0
chrismcg61 <31438040+chrismcg61@users.noreply.github.com>
2021-03-22 18:44:10 +0100

    Added Sky Shader Particle System

--- MyLibs/Three/My3D.js
+++ MyLibs/Three/My3D.js
@@ -53,4 +53,95 @@ function onGuiChange(){
 
 
 
+
+var vShaderSky, fShaderSky;
+{
+  vShaderSky = `
+    attribute vec3 color;
+    varying vec3 vColor;
+    uniform float time;
+    uniform float speed;
+    uniform float pointSize;
+    void main() {
+      vColor = color;
+      float aa = vColor.r;
+      //      
+      vec3 tmpPos = position;
+      vec4 mvPosition = modelViewMatrix * vec4( tmpPos, 1.0 );
+      //
+      gl_PointSize = pointSize * (1.0+2.0*aa);
+      if(pointSize<=1.0) gl_PointSize *= cos(time*speed*aa);
+      // gl_PointSize /= -mvPosition.z;
+      
+      gl_Position = projectionMatrix * mvPosition;
+    }
+    `;   
+  fShaderSky = `
+    uniform sampler2D pointTexture;
+    uniform float time;
+    uniform float speed;
+    uniform float alpha;
+    uniform float pointSize;
+    varying vec3 vColor;
+    void main() {      
+      float aa = vColor.r;
+      float aaa = cos(time*speed*aa);
+      float bb = 0.25*aa;
+      vec2 tmpPtCoord = gl_PointCoord - vec2(0.5,0.5);
+      tmpPtCoord = vec2(tmpPtCoord.x*cos(bb) + tmpPtCoord.y*sin(bb),  tmpPtCoord.y*cos(bb) + tmpPtCoord.x*sin(bb));
+      gl_FragColor = vec4( vColor, alpha );
+      if(pointSize>1.0) {        
+        gl_FragColor.a = alpha + aaa*0.5*alpha;
+        gl_FragColor *= texture2D( pointTexture, vec2(0.5,0.5)+tmpPtCoord );    
+      }
+      else{
+        float rr = length( gl_PointCoord - vec2( 0.5, 0.5 ) );
+        if ( rr > 0.5 ) discard;      //Draw Disc      
+      }
+    }
+    `;
+}
+function initGpuParticlesSky(pointNb, size, pointSize, _col, _colA){
+  // var vertices = new THREE.BoxGeometry( 50,50,50, 10,10,10 ).vertices;
+  var positions = new Float32Array( pointNb * 3 );
+  var colors = new Float32Array( pointNb * 3 );
+  for ( var i = 0, l = pointNb; i < l; i ++ ) {
+    var vertex = new THREE.Vector3( (Math.random()-0.5), Math.random()-0.5, (Math.random()-0.5) );
+    vertex.normalize().multiplyScalar(size);
+    vertex.toArray( positions, i * 3 );
+    var color = new THREE.Color( _col.r+Math.random()*_colA.r,  _col.g+Math.random()*_colA.g,  _col.b+Math.random()*_colA.b );
+    color.toArray( colors, i * 3 );
+  }
+  var geometry = new THREE.BufferGeometry();
+  geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+  geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
+  
+  var newShaderUniforms = 
+  {
+    time: {value:0.0},
+    speed: {value:0.02},
+    alpha: {value:0.9},
+    pointSize: {value:pointSize},
+    pointTexture: {value: pointTexture},
+  };
+  shaderUniformList.push( newShaderUniforms );
+  var shaderMaterialSky = new THREE.ShaderMaterial( {
+    uniforms: newShaderUniforms,
+    vertexShader: vShaderSky,
+    fragmentShader: fShaderSky,
+    transparent:true,
+    //alphaTest: 0.9,
+    blending: THREE.AdditiveBlending,
+    //depthTest: true,
+    depthWrite: false,
+  } );
+  
+  var particles = new THREE.Points( geometry, shaderMaterialSky );
+  //scene.add( particles );
+  return particles;
+}
+
+
+
+
 // export default MY3D;
